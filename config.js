f1ce6cce7602e9ee019476867216c9bac216b6c5
Nam <106749493+NamMai0205@users.noreply.github.com>
2022-06-02 20:35:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 const CONFIG = {
     titleWeb: "Dành cho người tôi yêu ",
-    introTitle: 'Đức Bùi',
-    introDesc: `b sửa được cái nhìn của em dành cho tôi k ??
-    .....Được.....`,
+    introTitle: 'Chính là e người đang nhìn màn hình đấy ',
+    introDesc: `CẬU CÓ NHỚ 
+    MÌNH ĐÃ BỎ QUÊN ĐIỀU GÌ KHÔNG ? 
+    NẾU KHÔNG ,TỚ SẼ NHẮC CHO CẬU NHỚ; ĐIỀU CẬU BỎ QUÊN CHÍNH LÀ TỚ,
+    ƯỚC MƠ CỦA CẬU`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng Trung đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng Trung đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng e đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng E đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu',
     btnNo: 'Không yêu',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi em , người tôi yêu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
